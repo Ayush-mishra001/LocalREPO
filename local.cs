23e51b335939215f6f1dc6ce5465fdc61dcdcf00
Ayush-mishra001 <ayushgpt4321@gmail.com>
2025-11-05 20:47:12 +0530

    Add new branch

--- local.cs
+++ local.cs
@@ -1,8 +1,11 @@
 internal class local
- {
+{
     public string MyProperty { get; set; }
     public string MyProperty2 { get; set; }
     public string MyProperty3 { get; set; }
+
+    public int MyProperty4 { get; set; }
+    public string MyProperty1 { get; set; } 
 
 
 
